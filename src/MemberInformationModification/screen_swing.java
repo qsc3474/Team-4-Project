8ecb92b0e04ff7191518c0edb4aab4df7eeda959
Administrator <Administrator@112.171.77.231>
2021-01-28 23:58:05 +0900

    20210128 11/57 최혁진
    GUI 비밀번호 수정

--- src/MemberInformationModification/screen_swing.java
+++ src/MemberInformationModification/screen_swing.java
@@ -1,13 +1,17 @@
 package MemberInformationModification;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
+import javax.imageio.ImageIO;
 import javax.swing.ActionMap;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,10 +24,22 @@ import javax.swing.JTextField;
 public class screen_swing extends JFrame{
 	
 
-	public screen_swing() {
-		//Modified mm = new Modified();
-
+public screen_swing() {
+		
+		JPanel panel2 = new JPanel();
 		JPanel panel = new JPanel();
+		this.setTitle("커피한잔의 여유");
+		
+		
+		
+		add(panel2);
+		add(panel);
+		
+		setVisible(true);//창보이게
+		setSize(400,300);//사이즈
+		setLocationRelativeTo(null);//가운데서 뜬다
+		setResizable(false);//사이즈 고정
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		panel.setLayout(null);
 		
@@ -32,12 +48,16 @@ public class screen_swing extends JFrame{
 		JLabel label2 = new JLabel("새로운 비밀번호  : ");
 		JLabel label3 = new JLabel("새로운 비밀번호  : ");
 		JButton button = new JButton("확인");// 확인버튼
+		JButton button2 = new JButton();// 확인버튼
 		
 		JPasswordField pwdText = new JPasswordField(20);
 		JPasswordField pwdText2 = new JPasswordField(20);
 		JPasswordField pwdText3 = new JPasswordField(20);
 		
-		panel.add(labe);
+		
+		panel2.add(labe);
+		panel2.add(button2);
+		
 		panel.add(label);
 		panel.add(pwdText);
 		panel.add(label2);
@@ -46,27 +66,29 @@ public class screen_swing extends JFrame{
 		panel.add(pwdText3);
 		panel.add(button);
 		
-		label.setBounds(50, 60, 110, 25);
-		labe.setBounds(145, 15, 110, 25);
+		
+		
+		button2.setBounds(10, 8, 30, 25);
 		label2.setBounds(50, 100, 110, 25);
+		labe.setBounds(145, 9, 110, 25);
+		panel2.setBackground(new Color(000, 051, 102));
+		panel2.setSize(400,40);
+		
+		label.setBounds(50, 60, 110, 25);
 		label3.setBounds(50, 140, 120, 25);
 		pwdText.setBounds(180, 60, 165, 25);
 		pwdText2.setBounds(180, 100, 165, 25);
 		pwdText3.setBounds(180, 140, 165, 25);
 		button.setBounds(150, 190, 80, 25);
+		labe.setForeground(Color.WHITE);
+		panel.setBackground(Color.WHITE);
+		try {
+			this.setIconImage(ImageIO.read(new File("images/asd.PNG")));
+		} catch (IOException e1) {
+			
+			e1.printStackTrace();
+		}
 		
-		
-		
-		
-		
-		
-		
-
-
-		
-		
-	
-
 		
 		
 		button.addActionListener(new ActionListener(){
@@ -92,14 +114,7 @@ public class screen_swing extends JFrame{
 				
 			}
 		});
-		add(panel);
 		
-		
-		setVisible(true);//창보이게
-		setSize(400,280);//사이즈
-		setLocationRelativeTo(null);//가운데서 뜬다
-		setResizable(false);//사이즈 고정
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 	public static void main(String[] args) {
