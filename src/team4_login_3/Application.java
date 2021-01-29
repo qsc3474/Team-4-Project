2a1e0cbfea7867075680b7249938e4e74f1fcf10
Administrator <Administrator@121.140.11.62>
2021-01-30 07:40:05 +0900

    2021 01 30 최혁진
    비밀번호 수정 완성

--- src/team4_login_3/Application.java
+++ src/team4_login_3/Application.java
@@ -3,6 +3,7 @@ package team4_login_3;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import MemberInformationModification.Modified;
 
 //import team4_login_1.MemberDTO;
 
@@ -10,8 +11,9 @@ public class Application {
 
 
 	public static void main(String[] args) {
-
+			
 		Scanner sc = new Scanner(System.in);
+		Modified modified = new  Modified();
 		Register register = new Register();		// 회원 회원가입 인스턴스
 		Login login = new Login();				// 회원 로그인 인스턴스
 
@@ -20,7 +22,7 @@ public class Application {
 				System.out.println("1. 회원가입");			// 선택 시 회원가입
 				System.out.println("2. 로그인");			// 선택 시 로그인
 				System.out.println("3. 회원조회");			// 선택 시 회원조회
-				System.out.println("4. 회원수정 및 탈퇴");			// 선택 시 회원조회
+				System.out.println("4. 회원수정 및 탈퇴");	// 선택 시 회원조회
 				System.out.println("9. 프로그램 종료");		// 선택 시 프로그램 종료
 				System.out.print("번호를 입력하세요 :");
 				int no = sc.nextInt();
@@ -37,6 +39,7 @@ public class Application {
 					break;
 				case 4 :
 //					EditInfo();					// 회원 수정 및 탈퇴
+					modified.passwordChange();
 					break;					
 				case 9 : System.out.println("프로그램을 종료합니다.");
 				return;
