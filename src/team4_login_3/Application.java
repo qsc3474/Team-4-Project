7d45a7556fb0befe749178680d7f27741fc7fc19
petercho0819 <dongr52@naver.com>
2021-01-29 15:51:52 +0900

    210129 로그인

--- src/team4_login_3/Application.java
+++ src/team4_login_3/Application.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import team4_login_1.MemberDTO;
+//import team4_login_1.MemberDTO;
 
 public class Application {
 
@@ -20,6 +20,7 @@ public class Application {
 				System.out.println("1. 회원가입");			// 선택 시 회원가입
 				System.out.println("2. 로그인");			// 선택 시 로그인
 				System.out.println("3. 회원조회");			// 선택 시 회원조회
+				System.out.println("4. 회원수정 및 탈퇴");			// 선택 시 회원조회
 				System.out.println("9. 프로그램 종료");		// 선택 시 프로그램 종료
 				System.out.print("번호를 입력하세요 :");
 				int no = sc.nextInt();
@@ -29,11 +30,14 @@ public class Application {
 					register.register_member();	// 회원가입 메소드 실행
 					break;
 				case 2 : 		// 로그인
-					login.Login_member();	// 로그인 메소드 실행
+					login.Login_member();		// 로그인 메소드 실행
 					break;
-				case 3 : 		// 회원정보 조회
-					//				search();
+				case 3 : 						// 회원정보 조회
+//					InfoCheck.info_member();
 					break;
+				case 4 :
+//					EditInfo();					// 회원 수정 및 탈퇴
+					break;					
 				case 9 : System.out.println("프로그램을 종료합니다.");
 				return;
 				default : System.out.println("잘못된 번호를 입력하셨습니다.");
